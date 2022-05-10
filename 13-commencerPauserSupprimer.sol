f65b608975e42a56a149000888720c86e7ea510f
unknown <l.boukorras@gmail.com>
2022-05-10 20:25:20 +0200

    learn - 10/05/22

--- 13-commencerPauserSupprimer.sol
+++ 13-commencerPauserSupprimer.sol
@@ -5,6 +5,8 @@ pragma solidity 0.8.13;
 contract Owner {
 
     address owner;
+    bool paused;
+    // bool est toujours à false
     uint nombre;
 
     constructor()  {
@@ -12,16 +14,27 @@ contract Owner {
         owner = msg.sender;
     }
 
-    function setNombre(uint _nombre) public {
+    function setPaused(bool _paused) public {
         // On verifie que l'utilisateur qui appelle la fonction est bien le owner
+        require (msg.sender == owner, "Vous n'etes pas le owner");
+        paused = _paused;
+    }
+
+    function setNombre(uint _nombre) public {
+        require(!paused, "Le contrat est en pause");
         require (msg.sender == owner, "Vous n'etes pas le owner");
         nombre = _nombre;
 
     }
 
     function getNombre() public view returns (uint) {
+        require(!paused, "Le contrat est en pause");
         return nombre;
     }
-    
+
+    function destroy(address payable _to) public {
+        require(msg.sender == owner, "Vous n'etes pas le owner");
+        selfdestruct(_to);
+
 
 }
